2e2c338302f7bb6f750e12f4bf63f3ece32f55c2
Administrator <506100377@qq.com>
2021-01-30 00:22:16 +0800

    Many change

--- Hello/src/HelloWorld.java
+++ Hello/src/HelloWorld.java
@@ -4,5 +4,7 @@ public class HelloWorld {
         System.out.println("Hello World");
         System.out.println("Hello World");
         System.out.println("Hello World");
+        System.out.println("GITHUB 艹尼玛");
+
     }
 }
---
+++ Hello/src/NewPackage/PrintFuck.java
@@ -0,0 +1,12 @@
+package NewPackage;
+
+public class PrintFuck {
+    private String statement;
+    public PrintFuck() {
+        this.statement = "Fuck!";
+    }
+
+    public void printStatment() {
+        System.out.println(this.statement);
+    }
+}
